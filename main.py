c35cb5af952543ee59be4235b740bccc0e15e83a
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-26 17:14:27 +0100

    Aktualisiere main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-led_an:False
+led_an=False
 while True:
     if input.button_is_pressed(Button.A):
         if led_an==True:
-            pass
+            basic.set_led_color(0x00500)
         else:
