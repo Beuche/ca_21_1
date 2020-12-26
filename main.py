f0afc62704b21f36a957e3937f1906cf6b77e5a2
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-26 17:16:20 +0100

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ led_an=False
 while True:
     if input.button_is_pressed(Button.A):
         if led_an==True:
-            basic.set_led_color(0x00500)
+            basic.set_led_color(0x005000)
+            led_an=False
         else:
+            basic.set_led_color(0x000000)
+            led_an=True
+    while not input.button_is_pressed(Button.A):
+        pass
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+let led_an = false
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        if (led_an == true) {
+            basic.setLedColor(0x005000)
+            led_an = false
+        } else {
+            basic.setLedColor(0x000000)
+            led_an = true
+        }
+        
+    }
+    
+    while (!input.buttonIsPressed(Button.A)) {
+        
+    }
+}
